4d5762dae5c2acf11137c5db3504e748d77e64f6
davidmanta7 <davidmontoya1998@gmail.com>
2019-01-19 20:48:38 -0500

    Cleaned up server side and connected to mongodb

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://admin:passw0rd@ds161794.mlab.com:61794/recsports-schedule-swamphacks'
+}
---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Body parser middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./../config/keys').mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Server started on port ${port}'));
