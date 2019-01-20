a00bd5e71a6931485a6819247f72d0a70def8810
Spencer <swongsen@ufl.edu>
2019-01-19 21:08:52 -0500

    yeat

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className = "App-TOP">
+          <h className = "App-header">
+            UF RecSports
+            </h>
+
+          <p className = "App-text">
+            pepeepee
+          </p>
+
+
+
+        </div>
+    );
+  }
+}
+
+
+
+//<a
+//  className="App-link"
+//  href="https://reddit.com" // Page destination
+//  target="_blank" //Open up new page
+//>
+//  Learn React
+//</a>
+
+export default App;
