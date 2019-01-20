9923a4ce3cd0e27b2b4dfdcbe058ddfbe16b53a1
Spencer <swongsen@ufl.edu>
2019-01-19 23:11:27 -0500

    lkj

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 
@@ -7,10 +8,28 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div className = "App-TOP">
+          <h className = "App-header">
+            UF RecSports
+            </h>
 
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+            <p className = "dropdown">
+              <button className = "App-Dropbtn">
+                Click me
+                </button>
+            <p className = "dropdown-content">
+
+            <p className = "App-DropCont">
+              <p>
+              FUCK
+              </p>
+            </p>
+            </p>
+          </p>
+
+
+
+        </div>
     );
   }
 }
