e16a7c7dcb3f194c7e4fb9fb6c1ba18d742c87fe
Alain Garcia <alaingarcia@ufl.edu>
2019-01-19 20:00:03 -0500

    Create listingModel.js

---
+++ server/listingModel.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var listingSchema = new Schema({
+    day: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    building: {
+        type: String,
+        required: true
+    },
+    room: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String
+    }
+});
